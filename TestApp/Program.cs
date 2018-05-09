81c7c407674fde769ea83b3a72caa231678a3e02
KM-Satoru-Takahashi <stakahashi@liberworks.co.jp>
2018-05-09 16:54:06 +0900

    new

---
+++ TestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestApp
+{
+    public class Program
+    {
+        private static readonly string HelloWorld = "Hello World";
+
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine(HelloWorld);
+
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+            //
+        }
+    }
+}
